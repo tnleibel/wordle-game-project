dbfff7763fe73250454a68c5cbacd0f923d15ecb
tnleibel <tnleibel@outlook.com>
2024-06-24 17:32:25 -0400

    Basic visuals added, 5x6 grid achieved

---
+++ app.js
@@ -0,0 +1,22 @@
+/*-------------------------------- Constants --------------------------------*/
+const rules = "Welcome to Wordle!\nThe goal of the game is to guess a secret five-letter word.\nYou have six tries to guess the secret word.\nEach time you submit a guess, the letter tiles in that row will flip!\nGrey letters are not in the secret word.\nYellow letters are in the secret word, but are in the wrong position within it.\nGreen letters are both in the secret word and in the correct position.\nFive green letters means you've correctly guessed the secret word!"
+console.log(rules)
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
